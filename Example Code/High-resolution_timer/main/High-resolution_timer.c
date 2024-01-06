4005a2b6cf082906412412fb4deb5d769f7266a9
Mads A. Pedersen <mads.amtoft@hotmail.com>
2024-01-06 14:08:46 +0100

    Adding examples

--- Example Code/High-resolution_timer/main/High-resolution_timer.c
+++ Example Code/High-resolution_timer/main/High-resolution_timer.c
@@ -9,30 +9,31 @@
 
 static const char *TAG = "";
 
-void primeFactor(long num);
+void primeFactor(long long num);
 
 void app_main(void) {
     int64_t tS = esp_timer_get_time(); 
 
-    primeFactor(12);
+    primeFactor(9223372036854775807);
 
     int64_t tE = esp_timer_get_time();
 
     int64_t diff = tE - tS;
 
-    ESP_LOGI(TAG, "It took %lu us to execute primeFactor()", (long) diff);
+    ESP_LOGI(TAG, "Elapsed time: %llu microseconds", (long long) diff);
 }
 
-void primeFactor(long num) {
-    ESP_LOGI(TAG, "PrimeFactors of %lu", num);
-    long i = 2;
+void primeFactor(long long num) {
+    ESP_LOGI(TAG, "PrimeFactors of %llu", num);
+    long long i = 2;
 
     while (i <= sqrt(num)){
         if(num % i == 0) {
             num /= i;
-            ESP_LOGI(TAG, "%lu is a factor", i);
+            ESP_LOGI(TAG, "%llu is a factor", i);
         } else {
             i++;
         }
     }
+    ESP_LOGI(TAG, "%llu is a factor", num);
 }
